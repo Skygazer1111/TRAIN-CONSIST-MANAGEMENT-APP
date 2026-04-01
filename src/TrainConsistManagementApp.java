e29fa02f0cebbb6f9604cf69b1bae55032332758
Priyan <pr9348@srmist.edu.in>
2026-04-01 09:19:31 +0530

    Remove grouping of bogies by type
    Removed the grouping of bogies by type and related output.

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,6 +1,5 @@
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Map;
 
 public class TrainConsistManagementApp {
 
@@ -8,7 +7,6 @@ public class TrainConsistManagementApp {
 
         System.out.println("============================");
         System.out.println("Total Train Capacity");
-        System.out.println("Group Bogies by Type");
         System.out.println("============================\n");
 
         List<Bogie> bogieList = new ArrayList<>();
@@ -17,23 +15,11 @@ public class TrainConsistManagementApp {
         bogieList.add(new Bogie("AC Chair", 56));
         bogieList.add(new Bogie("Sleeper", 70));
         bogieList.add(new Bogie("First Class", 24));
-        bogieList.add(new Bogie("AC Chair", 58));
 
         BogieService service = new BogieService();
 
         int totalCapacity = service.getTotalCapacity(bogieList);
 
         System.out.println("Total Seating Capacity: " + totalCapacity);
-
-        Map<String, List<Bogie>> grouped = service.groupByType(bogieList);
-
-        System.out.println("Grouped Bogies:\n");
-
-        for (Map.Entry<String, List<Bogie>> entry : grouped.entrySet()) {
-            System.out.println(entry.getKey() + ":");
-            for (Bogie b : entry.getValue()) {
-                System.out.println("  " + b);
-            }
-        }
     }
 }
