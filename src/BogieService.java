1ce740736a1a1e4a621bfca7d20ad7bb4219da80
Skygazer1111 <pr9348@srmist.edu.in>
2026-04-01 09:06:56 +0530

    UC9 :  Group Bogies by Type

--- src/BogieService.java
+++ src/BogieService.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class BogieService {
@@ -7,5 +8,10 @@ public class BogieService {
         return bogies.stream()
                 .filter(b -> b.getCapacity() > threshold)
                 .collect(Collectors.toList());
+    }
+
+    public Map<String, List<Bogie>> groupByType(List<Bogie> bogies) {
+        return bogies.stream()
+                .collect(Collectors.groupingBy(Bogie::getName));
     }
 }
