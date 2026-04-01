ec51e5fc9795a28b37e442bb65e9cbba421fc1d2
Skygazer1111 <pr9348@srmist.edu.in>
2026-04-01 09:15:07 +0530

    UC10: Count Total Seats in Train (reduce)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,30 +1,25 @@
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Comparator;
 
 public class TrainConsistManagementApp {
+
     public static void main(String[] args) {
 
         System.out.println("============================");
-        System.out.println("Sort Bogies by Capacity");
+        System.out.println("Total Train Capacity");
         System.out.println("============================\n");
 
         List<Bogie> bogieList = new ArrayList<>();
 
         bogieList.add(new Bogie("Sleeper", 72));
         bogieList.add(new Bogie("AC Chair", 56));
+        bogieList.add(new Bogie("Sleeper", 70));
         bogieList.add(new Bogie("First Class", 24));
 
-        System.out.println("Before Sorting:\n");
-        for (Bogie b : bogieList) {
-            System.out.println(b);
-        }
+        BogieService service = new BogieService();
 
-        bogieList.sort(Comparator.comparingInt(Bogie::getCapacity));
+        int totalCapacity = service.getTotalCapacity(bogieList);
 
-        System.out.println("\nAfter Sorting (by Capacity):\n");
-        for (Bogie b : bogieList) {
-            System.out.println(b);
-        }
+        System.out.println("Total Seating Capacity: " + totalCapacity);
     }
 }
