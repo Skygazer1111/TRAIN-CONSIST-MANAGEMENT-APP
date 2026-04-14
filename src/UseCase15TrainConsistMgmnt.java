ac5b7567a8b6a2b10c887db801c079968cecc042
Skygazer1111 <pr9348@srmist.edu.in>
2026-04-14 20:42:50 +0530

    UC15: Safe Cargo Assignment Using try-catch-finally

---
+++ src/UseCase15TrainConsistMgmnt.java
@@ -0,0 +1,19 @@
+public class UseCase15TrainConsistMgmnt {
+
+    public static void main(String[] args) {
+        System.out.println("====================================");
+        System.out.println("UC15 - Safe Cargo Assignment");
+        System.out.println("====================================");
+        System.out.println();
+
+        GoodsBogie cylindricalBogie = new GoodsBogie("Cylindrical");
+        cylindricalBogie.assignCargo("Petroleum");
+        System.out.println();
+
+        GoodsBogie rectangularBogie = new GoodsBogie("Rectangular");
+        rectangularBogie.assignCargo("Petroleum");
+        System.out.println();
+
+        System.out.println("UC15 runtime handling completed...");
+    }
+}
--- test/TestRunner.java
+++ test/TestRunner.java
@@ -11,12 +11,13 @@ public class TestRunner {
         Result result2 = JUnitCore.runClasses(PassengerBogieTest.class);
         Result result3 = JUnitCore.runClasses(ValidationServiceTest.class);
         Result result4 = JUnitCore.runClasses(SafetyServiceTest.class);
+        Result result5 = JUnitCore.runClasses(GoodsBogieTest.class);
         
         // Print summary
         System.out.println("\n=== Test Summary ===");
-        int totalTests = result1.getRunCount() + result2.getRunCount() + result3.getRunCount() + result4.getRunCount();
-        int totalFailures = result1.getFailureCount() + result2.getFailureCount() + result3.getFailureCount() + result4.getFailureCount();
-        int totalIgnored = result1.getIgnoreCount() + result2.getIgnoreCount() + result3.getIgnoreCount() + result4.getIgnoreCount();
+        int totalTests = result1.getRunCount() + result2.getRunCount() + result3.getRunCount() + result4.getRunCount() + result5.getRunCount();
+        int totalFailures = result1.getFailureCount() + result2.getFailureCount() + result3.getFailureCount() + result4.getFailureCount() + result5.getFailureCount();
+        int totalIgnored = result1.getIgnoreCount() + result2.getIgnoreCount() + result3.getIgnoreCount() + result4.getIgnoreCount() + result5.getIgnoreCount();
         
         System.out.println("Total Tests: " + totalTests);
         System.out.println("Failures: " + totalFailures);
@@ -32,6 +33,7 @@ public class TestRunner {
             printFailures(result2);
             printFailures(result3);
             printFailures(result4);
+            printFailures(result5);
         }
     }
     
